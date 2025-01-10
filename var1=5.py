96002ab1084790036ff3dc26c2abbac855447ec2
contaedfegc <conta.Desco.la@hotmail.com>
2025-01-10 16:26:52 +0000

    meu index 1

---
+++ var1=5.py
@@ -0,0 +1,35 @@
+var1=5
+dobro=var1*2
+print("O dobro",dobro)
+
+#%%
+print("Ola")
+# %%
+print("Dinis")
+# %%
+pi=3.14
+area=8
+base=4
+A = (area**2) * pi
+B = base * 2 * area
+C = pi + base + area
+D = (area - pi) / base
+print(f"O valor de A é: {A}")
+print(f"O valor de B é: {B}")
+print(f"O valor de C é: {C}")
+print(f"O valor de D é: {D}")
+x1=10
+y1=4
+x2=1.50
+y2=3.90
+k1=y1**(1/2)+x1-y1
+k2=x2*y1**2/y2
+k3=y2+x1-x2-y1
+k4=x1//y1+y2%x2
+k5=x2/y2-y1+x1
+print(f"O valor de k1 é: {k1}")
+print(f"O valor de k2 é: {k2}")
+print(f"O valor de k3 é: {k3}")
+print(f"O valor de k4 é: {k4}")
+print(f"O valor de k5 é: {k5}")
+
